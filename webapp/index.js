888b4d5b983baac46a4fdd504a4a619092fb36f4
fabriziobonelli <75737295+fabriziobonelli@users.noreply.github.com>
2021-01-11 10:19:48 +0100

    Controls
    Added a simple text sapui5 control.

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-
-], function() {
+    "sap/m/Text"
+], function(Text) {
     'use strict';
     
-    alert("UI5 is ready to go!");
+    new Text({
+        text: "Hello UI5! (from index.js)"
+    }).placeAt("content")
 });
